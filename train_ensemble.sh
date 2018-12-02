e502bf296a6d53b7f1c67b2be80e720cab2d2bdf
dhyeon <dhyeon.park@gmail.com>
2018-12-02 14:04:03 +0900

    bash files updated

--- train_ensemble.sh
+++ train_ensemble.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+# Train with 10 Models for Ensemble
+# 0 : smiles, 1: inchikey, 2: ecfp, 3: mol2vec
 python main.py --data-path './tasks/data/drug(v0.6).pkl' --model-name 'trained_model.mdl' --rep-idx 2 --perform-ensemble True
